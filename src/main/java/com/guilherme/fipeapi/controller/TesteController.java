63d0eae3913eca0af42567af3ee986a9a562b965
guilherme <guillher.naasc@gmail.com>
2026-05-14 13:44:19 -0300

    api dinamica com parametros na rota

--- src/main/java/com/guilherme/fipeapi/controller/TesteController.java
+++ src/main/java/com/guilherme/fipeapi/controller/TesteController.java
@@ -1,15 +1,22 @@
 package com.guilherme.fipeapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.guilherme.fipeapi.model.Carro;
 
 @RestController
 public class TesteController {
 
-    @GetMapping("/teste")
-    public String teste(){
-        return "API funcionando";
-        
+    @GetMapping("/teste/{nome}/{marca}")
+    public Carro teste(@PathVariable String nome, @PathVariable String marca){
+
+        Carro carro = new Carro();
+        carro.nome = nome;
+        carro.marca = marca;
+
+        return carro;
     }
-    
+  
 }
---
+++ src/main/java/com/guilherme/fipeapi/model/Carro.java
@@ -0,0 +1,6 @@
+package com.guilherme.fipeapi.model;
+
+public class Carro {
+    public String nome;
+    public String marca;
+}
