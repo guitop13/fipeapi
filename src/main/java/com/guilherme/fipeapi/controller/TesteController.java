6917abe92a25d2993f2f19e89dd564e5b414c5d7
guilherme <guillher.naasc@gmail.com>
2026-05-22 00:29:12 -0300

    adicionados os comentarios

--- src/main/java/com/guilherme/fipeapi/controller/TesteController.java
+++ src/main/java/com/guilherme/fipeapi/controller/TesteController.java
@@ -1,7 +1,12 @@
 package com.guilherme.fipeapi.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;/*ele vai criar rotas para acessar meus metodos
+exemplo: @GetMapping("/cep") quando for chamado cep acessa esse metodo*/
+import org.springframework.web.bind.annotation.RestController; /* annotion.RestController= 
+uma etiqueta que vai fazer com que minha classe converse na web/API
+ela indica que uma classe responda requisições http
+pensa assim: vai ter uma requisição http, o spring vai procura algo que tenha a etiqueta(RestController)-
+que que atenda ao pedido, o spring vai pegar tranforma em http e enviar o chamado*/
 import org.springframework.web.client.RestTemplate;
 /*responsavel por buscar informações em outras api
 nesse caso é tipo assim: restTemplate.getForObject(...) significa olha busca(get) informações desse local
