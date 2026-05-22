06effa2e693d160f843785e546cf09b7bdb1333a
guilherme <guillher.naasc@gmail.com>
2026-05-22 00:20:04 -0300

    api externa comunição

--- src/main/java/com/guilherme/fipeapi/controller/TesteController.java
+++ src/main/java/com/guilherme/fipeapi/controller/TesteController.java
@@ -1,22 +1,27 @@
 package com.guilherme.fipeapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.guilherme.fipeapi.model.Carro;
+import org.springframework.web.client.RestTemplate;
+/*responsavel por buscar informações em outras api
+nesse caso é tipo assim: restTemplate.getForObject(...) significa olha busca(get) informações desse local
+e mostra pra mim: abaixo voce se deparara com String.class, que pe basicamente indicando qual a forma de resposat quero obter
+nesse caso string*/
 
 @RestController
 public class TesteController {
 
-    @GetMapping("/teste/{nome}/{marca}")
-    public Carro teste(@PathVariable String nome, @PathVariable String marca){
+    RestTemplate restTemplate = new RestTemplate();
 
-        Carro carro = new Carro();
-        carro.nome = nome;
-        carro.marca = marca;
+    @GetMapping("/cep")
+    public String buscarCep() {
 
-        return carro;
+        String resultado = restTemplate.getForObject(
+            "https://viacep.com.br/ws/72543506/json/",
+            String.class
+        );
+
+        return resultado;
     }
-  
+
 }
